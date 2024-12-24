39109c38e8b94187e3c7f854f1072d3817261d6a
Amletick <VadimV2001@gmail.com>
2024-12-24 05:07:51 +0300

    media_fixed

--- recipe_site/recipe_site/settings.py
+++ recipe_site/recipe_site/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# Media files
+MEDIA_URL = '/media/'  # URL для доступа к медиа-файлам
+MEDIA_ROOT = BASE_DIR / 'media'  # Локальный путь для хранения медиа-файлов
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
